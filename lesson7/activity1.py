58bb2fe5e67d962b323dc915df4c16faf5ba9144
akshajmsm2027 <akshaj.msm2027@gmail.com>
2025-10-14 19:59:52 +0530

    lesson7 done

---
+++ lesson7/activity1.py
@@ -0,0 +1,10 @@
+my_tuple=()
+print(my_tuple)
+my_tuple=(1,2,3)
+print(my_tuple)
+my_tuple=(1,"Hello",3.4)
+print(my_tuple)
+print("Sliced :", my_tuple[1:4])
+for letter in (my_tuple):
+    print("Hello",letter)
+    
