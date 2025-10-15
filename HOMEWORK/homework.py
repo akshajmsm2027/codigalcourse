2ed53f47bdf3c8fcab701a5e79b21d023dcccb23
akshajmsm2027 <akshaj.msm2027@gmail.com>
2025-10-15 18:04:35 +0530

    eligiblety check

---
+++ HOMEWORK/homework.py
@@ -0,0 +1,7 @@
+fee=int(input ("Enter the amount of fees you paid \n"))
+if fee==29999:
+    print("You are eligble for your exam")
+else:
+    print("Sorry you are not eligible for the exam")
+    
+ 
